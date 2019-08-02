cf814c8d8c98c6911321efe81f8f0897cb692e81
Yifan Xu <jamesxu182@gmail.com>
2019-08-02 21:09:09 +0100

    update

--- 1org-network-goleveldb/composer/startFabric.sh
+++ 1org-network-goleveldb/composer/startFabric.sh
@@ -3,13 +3,16 @@
 # docker-compose -f "docker-compose.yml" down
 # docker-compose -f "docker-compose.yml" up -d
 
+env DOCKER_HOST=52.212.194.219:2375 docker-compose -f ./docker-compose-org1.yml up -d
+env DOCKER_HOST=34.243.133.22:2375 docker-compose -f ./docker-compose-orderer.yml up -d
+
 # wait for Hyperledger Fabric to start
 # incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
 # echo "sleeping for 10 seconds to wait for fabric to complete start up"
 # sleep 10
 
 # Create the channel
-docker exec peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c composerchannel -f /etc/hyperledger/configtx/composer-channel.tx
+# docker exec peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c composerchannel -f /etc/hyperledger/configtx/composer-channel.tx
 
 # Join peer0.org1.example.com to the channel.
-docker exec -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b composerchannel.block
+# docker exec -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b composerchannel.block
